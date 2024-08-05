0f7598cf3670dc418d44a5bcdd373b025e1e96a1
tangxs0808 <60935775+tangxs0808@users.noreply.github.com>
2024-08-05 15:12:50 +0800

    Update config.go

--- config.go
+++ config.go
@@ -1,50 +1,66 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/rs/zerolog/log"
-	"github.com/spf13/viper"
+    "fmt"
+    "github.com/rs/zerolog/log"
+    "github.com/spf13/viper"
 )
 
 type Config struct {
-	DB DBConfig
-	WX WXConfig
+    DB DBConfig
+    WX WXConfig
 }
 
 type WXConfig struct {
-	URL       string
-	AppId     string
-	AppSecret string
+    URL       string
+    AppId     string
+    AppSecret string
 }
+
 type DBConfig struct {
-	Path string
+    Path string
 }
 
 func GetConfig() (*Config, error) {
-	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
-	if err := viper.ReadInConfig(); err != nil {
-		log.Warn().Err(err).Msg("Failed to read configuration from disk")
+    viper.SetConfigName("config")
+    viper.AddConfigPath(".")
+    
+    // Read environment variables
+    viper.AutomaticEnv()
+    
+    // Set environment variable prefixes if needed
+    viper.SetEnvPrefix("MYAPP") // Change "MYAPP" to your desired prefix
 
-		return nil,fmt.Errorf("fatal error reading config file: %w", err)
-	}
+    // Read in the configuration file
+    if err := viper.ReadInConfig(); err != nil {
+        log.Warn().Err(err).Msg("Failed to read configuration from disk")
+        return nil, fmt.Errorf("fatal error reading config file: %w", err)
+    }
 
-	wxUrl := viper.GetString("weixin.url")
-	wxAppId := viper.GetString("weixin.app_id")
-	wxAppKey := viper.GetString("weixin.app_secret")
+    // Read values from config file and environment variables
+    wxUrl := viper.GetString("weixin.url")
+    wxAppId := viper.GetString("weixin.app_id")
+    wxAppKey := viper.GetString("weixin.app_secret")
 
-	dbPath := viper.GetString("db.path")
+    dbPath := viper.GetString("db.path")
 
-	config := Config{
-		DB: DBConfig{
-			Path: dbPath,
-		},
-		WX: WXConfig{
-			URL:       wxUrl,
-			AppId:     wxAppId,
-			AppSecret: wxAppKey,
-		},
-	}
-	return &config, nil
+    // Override values with environment variables if set
+    if envAppId := viper.GetString("MYAPP_WEIXIN_APP_ID"); envAppId != "" {
+        wxAppId = envAppId
+    }
+    if envAppSecret := viper.GetString("MYAPP_WEIXIN_APP_SECRET"); envAppSecret != "" {
+        wxAppKey = envAppSecret
+    }
+
+    config := Config{
+        DB: DBConfig{
+            Path: dbPath,
+        },
+        WX: WXConfig{
+            URL:       wxUrl,
+            AppId:     wxAppId,
+            AppSecret: wxAppKey,
+        },
+    }
+    return &config, nil
 }
